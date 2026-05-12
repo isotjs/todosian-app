62fd7daefc42c881e95002486ed1c37871f36e9a
İsmail TİRYAKİ <179320252+isotjs@users.noreply.github.com>
2026-05-12 16:11:11 +0100

    bump the version
    * Update title.txt
    * update: versionCode and versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.isotjs.todosian"
         minSdk = 30
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.5"
+        versionCode = 6
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
